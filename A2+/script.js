e6322d231644807a4886cd4c05c926e4460e9f76
Sterben <sergey.matushkin.98@mail.ru>
2024-09-28 11:25:54 +0300

    fix: moved the check of spaces only

--- A2+/script.js
+++ A2+/script.js
@@ -6,12 +6,12 @@ const removingSpacesFromAString = (string) => {
     while(start <= end && string[start] == ' ') {
         start++
     }
-    while(end >= start && string[end] == ' ') {
-        end--
-    }
     if (start > end) {
         console.log("Строка состоит только из пробелов.");
         return '';
+    }
+    while(end >= start && string[end] == ' ') {
+        end--
     }
     if (start === 0 && end === string.length - 1) {
         console.log("Нет пробелов в начале и в конце строки.");
