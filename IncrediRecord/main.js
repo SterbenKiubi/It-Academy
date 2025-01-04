65f0da1ca24758f8d39a8ad9f0c88e5b392f5b6d
Sterben <sergey.matushkin.98@mail.ru>
2025-01-04 11:53:26 +0300

    remove aos library and made all with css animation and create second page layout

---
+++ IncrediRecord/main.js
@@ -0,0 +1,8 @@
+const buttonPlay = document.getElementById('button-play');
+
+const buttonPlayClickHandler = () => {
+    window.location.href = 'secondPage/secondPage.html'
+}
+
+
+buttonPlay.addEventListener('click', buttonPlayClickHandler);
